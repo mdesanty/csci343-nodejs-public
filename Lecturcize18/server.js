46730c57e76694e4cf940639eabacffa72b346a8
Michael DeSanty <michael.desanty@mcla.edu>
2020-04-01 21:08:52 -0400

    Lecturcize18: We tried it

--- Lecturcize18/server.js
+++ Lecturcize18/server.js
@@ -1,55 +1,59 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-// install mysql module first using 'npm install mysql'
-var mysql = require('mysql');
+// install mysql module first using "npm install mysql"
+const mysql = require("mysql");
 
 // added so we can serve index.html...
-var http = require('http');
-var fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
-const conInfo = 
+const conInfo =
 {
-    host: process.env.IP,
-    user: process.env.C9_USER,
+    host: "localhost",
+    user: "root",
     password: "",
     database: "SONGDB"
 };
 
-var session = require('express-session'); 
-app.use(session({ secret: 'happy jungle', 
-                  resave: false, 
-                  saveUninitialized: false, 
-                  cookie: { maxAge: 60000 }}))
+const session = require("express-session");
+const sessionOptions =
+{
+  secret: "happy jungle",
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 60000 }
+}
+app.use(session(sessionOptions));
 
-app.get('/', serveIndex);                  
-app.get('/list', list);                  
-app.get('/add', add);
-app.get('/remove', remove);
-app.get('/clear', clear);
+app.get("/", serveIndex);
+app.get("/list", list);
+app.get("/add", add);
+app.get("/remove", remove);
+app.get("/clear", clear);
 
-app.listen(process.env.PORT,  process.env.IP, startHandler())
+app.listen(3000, "localhost", startHandler());
 
 function startHandler()
 {
-  console.log('Server listening on port ' + process.env.PORT)
+  console.log("Server listening on port 3000");
 }
 
 function list(req, res)
 {
-  var con = mysql.createConnection(conInfo);
-  con.connect(function(err) 
+  let con = mysql.createConnection(conInfo);
+  con.connect(function(err)
   {
-    if (err) 
-      writeResult(req, res, {'error' : err});
+    if (err)
+      writeResult(req, res, {error: err});
     else
     {
-      con.query("SELECT * FROM SONG ORDER BY SONG_NAME", function (err, result, fields) 
+      con.query("SELECT * FROM SONG ORDER BY SONG_NAME", function (err, result)
       {
-        if (err) 
-          writeResult(req, res, {'error' : err});
+        if (err)
+          writeResult(req, res, {error: err});
         else
-          writeResult(req, res, {'result' : result});
+          writeResult(req, res, {result: result});
       });
     }
   });
@@ -58,28 +62,28 @@ function list(req, res)
 function add(req, res)
 {
   if (req.query.song == undefined)
-    writeResult(req, res, {'error' : "add requires you to enter a song"});
+    writeResult(req, res, {error: "add requires you to enter a song"});
   else
   {
-    var con = mysql.createConnection(conInfo);
-    con.connect(function(err) 
+    let con = mysql.createConnection(conInfo);
+    con.connect(function(err)
     {
-      if (err) 
-        writeResult(req, res, {'error' : err});
+      if (err)
+        writeResult(req, res, {error: err});
       else
       {
-        con.query('INSERT INTO SONG (SONG_NAME) VALUES (?)', [req.query.song], function (err, result, fields) 
+        con.query("INSERT INTO SONG (SONG_NAME) VALUES (?)", [req.query.song], function (err, result)
         {
-          if (err) 
-            writeResult(req, res, {'error' : err});
+          if (err)
+            writeResult(req, res, {error : err});
           else
           {
-            con.query("SELECT * FROM SONG ORDER BY SONG_NAME", function (err, result, fields) 
+            con.query("SELECT * FROM SONG ORDER BY SONG_NAME", function (err, result)
             {
-              if (err) 
-                writeResult(req, res, {'error' : err});
+              if (err)
+                writeResult(req, res, {error: err});
               else
-                writeResult(req, res, {'result' : result});
+                writeResult(req, res, {result: result});
             });
           }
         });
@@ -91,28 +95,28 @@ function add(req, res)
 function remove(req, res)
 {
   if (req.query.song == undefined)
-    writeResult(req, res, {'error' : "add requires you to enter a song"});
+    writeResult(req, res, {error: "add requires you to enter a song"});
   else
   {
-    var con = mysql.createConnection(conInfo);
-    con.connect(function(err) 
+    let con = mysql.createConnection(conInfo);
+    con.connect(function(err)
     {
-      if (err) 
-        writeResult(req, res, {'error' : err});
+      if (err)
+        writeResult(req, res, {error: err});
       else
       {
-        con.query('DELETE FROM SONG WHERE SONG_NAME = ?', [req.query.song], function (err, result, fields) 
+        con.query("DELETE FROM SONG WHERE SONG_NAME = ?", [req.query.song], function (err, result)
         {
-          if (err) 
-            writeResult(req, res, {'error' : err});
+          if (err)
+            writeResult(req, res, {error: err});
           else
           {
-            con.query("SELECT * FROM SONG ORDER BY SONG_NAME", function (err, result, fields) 
+            con.query("SELECT * FROM SONG ORDER BY SONG_NAME", function (err, result)
             {
-              if (err) 
-                writeResult(req, res, {'error' : err});
+              if (err)
+                writeResult(req, res, {error: err});
               else
-                writeResult(req, res, {'result' : result});
+                writeResult(req, res, {result: result});
             });
           }
         });
@@ -123,25 +127,25 @@ function remove(req, res)
 
 function clear(req, res)
 {
-  var con = mysql.createConnection(conInfo);
-  con.connect(function(err) 
+  let con = mysql.createConnection(conInfo);
+  con.connect(function(err)
   {
-    if (err) 
-      writeResult(req, res, {'error' : err});
+    if (err)
+      writeResult(req, res, {error: err});
     else
     {
-      con.query('DELETE FROM SONG', function (err, result, fields) 
+      con.query("DELETE FROM SONG", function (err, result)
       {
-        if (err) 
-          writeResult(req, res, {'error' : err});
+        if (err)
+          writeResult(req, res, {error: err});
         else
         {
-          con.query("SELECT * FROM SONG ORDER BY SONG_NAME", function (err, result, fields) 
+          con.query("SELECT * FROM SONG ORDER BY SONG_NAME", function (err, result)
           {
-            if (err) 
-              writeResult(req, res, {'error' : err});
+            if (err)
+              writeResult(req, res, {error: err});
             else
-              writeResult(req, res, {'result' : result});
+              writeResult(req, res, {result: result});
           });
         }
       });
@@ -151,22 +155,14 @@ function clear(req, res)
 
 function writeResult(req, res, obj)
 {
-  if (req.session.commandCount == undefined)
-    req.session.commandCount = 0;
-
-  req.session.commandCount++;
-
-  obj.commandCount = req.session.commandCount;
-  
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  res.write(JSON.stringify(obj));
-  res.end('');
+  res.writeHead(200, {"Content-Type": "application/json"});
+  res.end(JSON.stringify(obj));
 }
 
 // added so we can serve index.html...
 function serveIndex(req, res)
 {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  var index = fs.readFileSync('index.html');
+  res.writeHead(200, {"Content-Type": "text/html"});
+  var index = fs.readFileSync("index.html");
   res.end(index);
 }
